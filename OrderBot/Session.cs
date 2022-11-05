b79084ab73f38f79ef25db51f684dee20e658c4c
keerthika2712 <keerthika66@gmail.com>
2022-11-04 20:30:43 -0400

    Made changes to the user and use cases

--- OrderBot/Session.cs
+++ OrderBot/Session.cs
@@ -6,7 +6,7 @@ namespace OrderBot
     {
         private enum State
         {
-            WELCOMING, LOCATION, PICKUPDATE
+            WELCOMING, LOCATION, PICKUPDATE, RETURNDATE, AVAILABLECARS, BOOKING , PAYMENT
         }
 
         private State nCur = State.WELCOMING;
@@ -31,15 +31,29 @@ namespace OrderBot
                 case State.LOCATION:
                     this.oOrder.Size = sInMessage;
                     this.oOrder.Save();
-                    aMessages.Add("choose locations (1. London 2. Waterloo 3. Kitchener)" + this.oOrder.Size + " London?");
+                    aMessages.Add("choose locations (1. London 2. Waterloo 3. Kitchener)" + this.oOrder.Size + " ");
                     this.nCur = State.PICKUPDATE;
                     break;
                 case State.PICKUPDATE:
-                    string sProtein = sInMessage;
-                    aMessages.Add("Choose pickup Date and Time" + this.oOrder.Size + " " + sProtein + " NOV 5th, 8:30 PM ?");
+                    string sPickupdate = sInMessage;
+                    aMessages.Add("Choose pickup Date and Time" + this.oOrder.Size + " " + sPickupdate + " ");
                     break;
-
-
+                case State.RETURNDATE:
+                    string sReturndate = sInMessage;
+                    aMessages.Add("Choose return Date and Time" + this.oOrder.Size + " " + sReturndate + " ");
+                    break;
+                case State.AVAILABLECARS:
+                    string sAvailablecars = sInMessage;
+                    aMessages.Add("Choose Available Cars (1. BENZ 2. KIA 3. SKODA 4. TOYOTA " + this.oOrder.Size + " " + sAvailablecars + " ");
+                    break;
+                case State.BOOKING:
+                    string sBooking = sInMessage;
+                    aMessages.Add("Confirm Booking " + this.oOrder.Size + " " + sBooking + " ");
+                    break;
+                case State.PAYMENT:
+                    string sPayment = sInMessage;
+                    aMessages.Add("Make Payment " + this.oOrder.Size + " " + sPayment + " ");
+                    break;
             }
             aMessages.ForEach(delegate (String sMessage)
             {
